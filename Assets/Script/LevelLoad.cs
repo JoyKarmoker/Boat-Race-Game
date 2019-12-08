6d95862d90e0af17a2c06227bb0b41754c7e883b
Joy Karmoker <joykarmoker353@gmail.com>
2019-12-08 13:22:52 +0600

    Main menu scene ready and Music manager created

--- Assets/Script/LevelLoad.cs
+++ Assets/Script/LevelLoad.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoad : MonoBehaviour
 {
-    [SerializeField] int timetoWait = 4;
+    //[SerializeField] int timetoWait = 4;
+    public Slider slider;
+    public Text progrssText;
     int currentSceneIndex;
     // Start is called before the first frame update
     void Start()
@@ -13,20 +16,60 @@ public class LevelLoad : MonoBehaviour
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         if(currentSceneIndex == 0)
         {
-            StartCoroutine(WaitForTime());
+            StartCoroutine(LoadAsynchronusly(currentSceneIndex));
         }
     }
 
-    IEnumerator WaitForTime()
+    IEnumerator LoadAsynchronusly(int sceneIndex)
     {
-        yield return new WaitForSeconds(timetoWait);
-        LoadNextScene();
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex+1);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress/0.9f);
+            int percentage = (int)(progress*100);
+            //Debug.Log(progress);
+            slider.value = progress;
+            progrssText.text = "Loading " +percentage+ "%";
+            yield return null;
+        }
+        
+        //LoadNextScene();
     }
 
-    public void LoadNextScene()
+     void LoadNextScene(int nextLevel)
     {
-        SceneManager.LoadScene(currentSceneIndex+1);
+        SceneManager.LoadScene(nextLevel);
     }
+
+    public void playButton()
+    {
+        
+        LoadNextScene(2);
+    }
+
+    public void settingsButton()
+    {
+        
+        LoadNextScene(3);
+    }
+
+    public void instructionButton()
+    {
+        
+        LoadNextScene(4);
+    }
+    public void aboutUsButton()
+    {
+        
+        LoadNextScene(5);
+    }
+
+    public void quitButton()
+    {
+        
+         Application.Quit();;
+    }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Script/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip[] levelMusicChangeArray;
+    private AudioSource audioSource;
+    
+    void  Awake() {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't Destroy on Load "+name);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+        //Debug.Log("Playing music " +thisLevelMusic);
+        if(thisLevelMusic) // If there is some music
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
