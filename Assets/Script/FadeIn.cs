d3242c695c8fb9a34941098cd7ab45d6c6d221df
Joy Karmoker <joykarmoker353@gmail.com>
2019-12-13 02:54:47 +0600

    Fade In Panel Added

---
+++ Assets/Script/FadeIn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    private float fadeInTime = 2f;
+    private Image fadeInPanel;
+    private Color currentColor = Color.black;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeInPanel = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.timeSinceLevelLoad < fadeInTime)
+        {
+            float alphaChange = Time.deltaTime / fadeInTime;
+            currentColor.a -= alphaChange;
+            fadeInPanel.color = currentColor;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
