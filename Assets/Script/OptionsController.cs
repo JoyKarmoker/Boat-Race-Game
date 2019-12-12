03cdafb3f4c530bb33ea2cfa3f521678b1305386
Joy Karmoker <joykarmoker353@gmail.com>
2019-12-13 05:06:27 +0600

    Player Prefs is not working properly

---
+++ Assets/Script/OptionsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private MusicManager musicManager;
+    float volumeOfMusic;
+    public Toggle toggle;
+
+    void Start()
+    {
+        musicManager = GameObject.FindObjectOfType<MusicManager>();
+        
+        volumeOfMusic = PlayerPrefsManager.GetMasterBackgroudMusic();
+        if(volumeOfMusic == 0f)
+        {
+            toggle.isOn = false;
+        }
+
+        else{
+            toggle.isOn = true;
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        if(toggle.isOn)
+        {
+           // PlayerPrefsManager.SetMasterBackgroudMusic(1f);
+           Debug.Log("SAve and exit button pressed, volume is "+PlayerPrefsManager.GetMasterBackgroudMusic());
+            SceneManager.LoadScene("01a Start");
+
+        }
+        else
+        {
+           // PlayerPrefsManager.SetMasterBackgroudMusic(0f);
+           Debug.Log("SAve and exit button pressed, volume is "+PlayerPrefsManager.GetMasterBackgroudMusic());
+             SceneManager.LoadScene("01a Start");
+
+        }
+    }
+
+    public void Save(bool IsMusicOn)
+    {
+        if(IsMusicOn)
+        {
+            PlayerPrefsManager.SetMasterBackgroudMusic(1f);
+        }
+        else
+        {
+            PlayerPrefsManager.SetMasterBackgroudMusic(0f);
+        }
+    }
+}
---
+++ Assets/Script/PlayerPrefsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    const string MASTER_BACKGROUNDMUSIC_KEY = "master_backgroundMusic";
+    public static void SetMasterBackgroudMusic(float volume)
+    {
+        if(volume >=0f && volume <=1f)
+        {
+             PlayerPrefs.SetFloat(MASTER_BACKGROUNDMUSIC_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Volume Out OF range");
+        }
+       
+    }
+
+    public static float GetMasterBackgroudMusic()
+    {
+       return( PlayerPrefs.GetFloat(MASTER_BACKGROUNDMUSIC_KEY));
+    }
+ 
+}
